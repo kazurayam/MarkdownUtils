fa851f91c5b1a19cae21fce519a87449ccef6981
kazurayam <kazuaki.urayama@gmail.com>
2022-04-14 14:30:22 +0900

    TranslateTOCTest#test_translateDocument_overwrite_file() passes

---
+++ build/tmp/testOutput/com.kazurayam.markdownutil.TranslateTOCTest/test_translateDocument_overwrite_file/README.md
@@ -0,0 +1,14 @@
+- [My Document](#my-document)
+    - [Section 1](#section-1)
+
+# My Document
+
+## Section 1
+
+Rainy today.
+
+```
+    - [foo](#_bar_baz)
+```
+
+
--- src/test/java/com/kazurayam/markdownutil/TranslateTOCTest.java
+++ src/test/java/com/kazurayam/markdownutil/TranslateTOCTest.java
@@ -1,38 +1,43 @@
 package com.kazurayam.markdownutil;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TranslateTOCTest {
 
     private Logger logger = LoggerFactory.getLogger(TranslateTOCTest.class);
 
-    private static Path fixtureDir =
-            Paths.get(System.getProperty("user.dir"))
-                    .resolve("src/test/resources/fixtures");
+    private static Path projectDir = Paths.get(System.getProperty("user.dir"));
+    private static Path fixtureDir = projectDir.resolve("src/test/resources/fixtures");
+
+    private static Path classOutputDir = projectDir.resolve("build/tmp/testOutput")
+                    .resolve(TranslateTOCTest.class.getName());
 
     @Test
-    public void test_translateDocument() throws Exception {
+    public void test_translateDocument_noOverwrite() throws Exception {
         Path sample = fixtureDir.resolve("README.md");
-        Reader reader =
-                new InputStreamReader(
+        Reader reader = new InputStreamReader(
                         new FileInputStream(sample.toFile()), StandardCharsets.UTF_8);
         Writer writer = new StringWriter();
         TranslateTOC.translateDocument(reader, writer);
@@ -43,6 +48,40 @@ public class TranslateTOCTest {
                 "the content does not contain a string \"#my-document\"");
         assertTrue(content.contains("#_bar_baz"),
                 "the content does not contain a string \"#_bar_baz\"");
+    }
+    @Test
+    public void test_translateDocument_overwrite_file() throws Exception {
+        Path inputFile = fixtureDir.resolve("README.md");
+        Path dir = classOutputDir.resolve("test_translateDocument_overwrite_file");
+        Files.createDirectories(dir);
+        Path workFile = dir.resolve("README.md");
+        Files.copy(inputFile, workFile, StandardCopyOption.REPLACE_EXISTING);
+        String content = readAllLines(workFile);
+        assertTrue(content.contains("(#_my_document)"));
+        assertFalse(content.contains("#my-document)"), "found unexpected #my-document");
+        assertTrue(content.contains("(#_bar_baz)"));
+        //
+        String relativePath = projectDir.relativize(workFile).toString();
+        //System.out.println("relativePath=" + relativePath);
+        assertTrue(relativePath.startsWith("build/tmp"), "relativePath=" + relativePath);
+        TranslateTOC.translateDocument(relativePath);
+        //
+        content = readAllLines(workFile);
+        assertFalse(content.contains("(#_my_document)"), "found unexpected #_my_document");
+        assertTrue(content.contains("#my-document)"));
+        assertTrue(content.contains("(#_bar_baz)"));
+    }
+
+    private String readAllLines(Path p) throws IOException {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        List<String> lines = Files.readAllLines(p);
+        for (String line : lines) {
+            pw.println(line);
+        }
+        pw.flush();
+        pw.close();
+        return sw.toString();
     }
 
     @Test
