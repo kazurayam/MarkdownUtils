f9a138f4763a83369e0ada3297701470bc836ca5
kazurayam <kazuaki.urayama@gmail.com>
2022-04-14 13:23:52 +0900

    add TranslateTOC#translateDocument(Reader, Writer)

--- src/main/java/com/kazurayam/markdownutil/TranslateTOC.java
+++ src/main/java/com/kazurayam/markdownutil/TranslateTOC.java
@@ -2,10 +2,21 @@ package com.kazurayam.markdownutil;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,7 +24,8 @@ import java.util.regex.Pattern;
 
 public class TranslateTOC {
 
-    private TranslateTOC() {}
+    private TranslateTOC() {
+    }
 
     static final String PTN_LEAD = "\\s*-\\s+";
     static final String PTN_TITLE = "[^\\]]*";
@@ -37,13 +49,13 @@ public class TranslateTOC {
      * I have a line:
      *
      * <PRE>
-     *     -    [My previous solution](#_my_previous_solution)
+     * -    [My previous solution](#_my_previous_solution)
      * </PRE>
-     *
+     * <p>
      * I want to translate this into
      *
      * <PRE>
-     *     -    [My previous solution](#my-previous-solution)
+     * -    [My previous solution](#my-previous-solution)
      * </PRE>
      *
      * @param line a line from README.md file
@@ -78,4 +90,33 @@ public class TranslateTOC {
         pw.flush();
     }
 
+    public static void translateDocument(String filePath) throws IOException {
+        Path cwd = Paths.get(System.getProperty("user.dir"));
+        Path input = cwd.resolve(filePath);
+        if (!Files.exists(input)) {
+            throw new FileNotFoundException(input.toString() + " is not found");
+        }
+        Reader reader = new InputStreamReader(
+                new FileInputStream(input.toFile()), StandardCharsets.UTF_8);
+        Path tempFile = Files.createTempFile(TranslateTOC.class.getSimpleName(), "");
+        Writer writer = new OutputStreamWriter(
+                new FileOutputStream(tempFile.toFile()), StandardCharsets.UTF_8);
+        // translate the input into the temporary file
+        TranslateTOC.translateDocument(reader, writer);
+        reader.close();
+        writer.flush();
+        writer.close();
+        // copy the translated text over the original
+        Files.copy(tempFile, input, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+
+    /**
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+    }
 }
